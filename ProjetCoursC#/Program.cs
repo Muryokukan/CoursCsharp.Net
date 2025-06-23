8cdf03a9c23182eaebdda4dab525731909eb2ebc
Cédric <134500848+Muryokukan@users.noreply.github.com>
2025-06-23 21:12:29 +0200

    daily: exo 5 done

---
+++ ProjetCoursC#/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] entier = { 13, 31, 59 };
+
+        
+
+        foreach (int n in entier)
+        {
+            Console.Write(n + " ");
+        }
+
+        List<string> prenoms = new List<string> { "othman", "jean", "cedric", "omar" };
+        Console.WriteLine("\n");
+        int indexOf = 0;
+
+        foreach (string p in prenoms)
+        {
+            Console.WriteLine(p + " " + indexOf);
+            indexOf++;
+        }
+
+        Console.WriteLine("\n");
+
+        Dictionary<string, string> pays = new Dictionary<string, string>
+        {
+            {"France", "Paris"},
+            { "Japon", "Tokyo"},
+            {"Italie", "Rome"},
+            {"Allemagne", "Berlin"}
+            
+        };
+
+        foreach (var entry in pays)
+        {
+            Console.WriteLine(entry.Key + " : " + entry.Value);
+        }
+
+        static void DireBonjour()
+        {
+            Console.WriteLine("Bonjour !");
+        }
+
+        DireBonjour();
+
+    }
+}
